8a68d82ce4ff180b59ecbbeda8ae3b01a3f0aa1b
Pascal Hauser <pascal.hauser457@gmail.com>
2020-12-03 16:13:55 +0100

    before week09

---
+++ Courseware/Le09-DynamicTypes/caseStudyFiles/DynamicWorldTest_Lab9.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerTest
+{
+    [TestClass]
+    public class DynamicWorldTest
+    {
+
+        [TestMethod]
+        public void TestDynamicWorld()
+        {
+            var cities = new Cities();
+
+            cities.ReadCities("citiesTestDataLab2.txt");
+
+            dynamic world = new World(cities);
+
+            dynamic karachi = world.Karachi();
+            Assert.AreEqual("Karachi", karachi.Name);
+            Assert.AreEqual(cities["Karachi"], karachi);
+
+            string notFound = world.Entenhausen();
+            Assert.AreEqual("The city \"Entenhausen\" does not exist!", notFound);
+        }
+    }
+}
